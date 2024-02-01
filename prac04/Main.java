54cd57f1d2e6c8bd72504c81555296c660defb48
wcarlosc <wcarlosc@unsa.edu.pe>
2024-01-31 23:50:19 -0500

    delete .class

--- prac04/Main.java
+++ prac04/Main.java
@@ -1,3 +1,4 @@
+/*
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -8,75 +9,100 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.input.MouseEvent;
 import javafx.event.EventHandler;
-
-public class Main extends Application{
+*/
+public class Main /* extends Application */ {
     public static void main(String[] args) {
-        launch(args);
+        // boton partida rapida
+        partidaRapida();
+        // boton partida personalizada
+        // partidaPersonalizada();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button("JUGAR");
-        primaryStage.setTitle("INICIO DEL JUEGO");
-
-        StackPane root = new StackPane();
-
-        // Cargar la imagen
-        Image backgroundImage = new Image(getClass().getResourceAsStream("/Img/Portada.jpg"));
-
-        // Crear un ImageView con la imagen
-        ImageView backgroundImageView = new ImageView(backgroundImage);
-
-        // Ajustar el tamaño preferido del StackPane para que coincida con el tamaño de la imagen
-        root.setPrefSize(backgroundImage.getWidth(), backgroundImage.getHeight());
-
-        // Añadir el ImageView al StackPane
-        root.getChildren().add(backgroundImageView);
-
-        // Aplicar estilos al botón
-        btn.setPrefSize(100, 60);
-        btn.setStyle(
-            "-fx-font-size: 18; " +
-            "-fx-background-color: #4CAF50; " +
-            "-fx-text-fill: white; " +
-            "-fx-border-color: #45a049; " +
-            "-fx-border-width: 2; " +
-            "-fx-border-radius: 5; " +
-            "-fx-background-radius: 5; " +
-            "-fx-padding: 10 20; " +
-            "-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.4), 10, 0, 0, 0);"
-           );
-           btn.setOnMouseEntered(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent e) {
-            btn.setStyle("-fx-background-color: #5EBE5E;"+ 
-            "-fx-font-size: 18; " + 
-            "-fx-text-fill: white; ");
-            }
-           });
-           btn.setOnMouseExited(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent e) {
-            btn.setStyle("-fx-background-color: #4CAF50;"+
-            "-fx-font-size: 18; " + 
-            "-fx-text-fill: white; ");
-            }
-           });
-
-        btn.setOnAction(event -> {  
-            Ventana v = new Ventana();
-            Ejercito e1 = new Ejercito(1);
-            Ejercito e2 = new Ejercito(2);
-            v.asignarAlTablero(e1);
-            v.asignarAlTablero(e2);
-        });
-
-        // Desplazar el botón "JUGAR" 110 píxeles hacia abajo
-        btn.setTranslateY(110);
-
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-
-        primaryStage.show();
+    public static void partidaRapida() {
+        Ventana v = new Ventana();
+        Ejercito e1 = new Ejercito(1);
+        Ejercito e2 = new Ejercito(2);
+        v.asignarAlTablero(e1);
+        v.asignarAlTablero(e2);
     }
+
+    public static void partidaPersonalizada() {
+
+    }
+    /*
+     * public static void main(String[] args) {
+     * launch(args);
+     * }
+     * 
+     * @Override
+     * public void start(Stage primaryStage) {
+     * Button btn = new Button("JUGAR");
+     * primaryStage.setTitle("INICIO DEL JUEGO");
+     * 
+     * StackPane root = new StackPane();
+     * 
+     * // Cargar la imagen
+     * Image backgroundImage = new
+     * Image(getClass().getResourceAsStream("/Img/Portada.jpg"));
+     * 
+     * // Crear un ImageView con la imagen
+     * ImageView backgroundImageView = new ImageView(backgroundImage);
+     * 
+     * // Ajustar el tamaño preferido del StackPane para que coincida con el tamaño
+     * de la imagen
+     * root.setPrefSize(backgroundImage.getWidth(), backgroundImage.getHeight());
+     * 
+     * // Añadir el ImageView al StackPane
+     * root.getChildren().add(backgroundImageView);
+     * 
+     * // Aplicar estilos al botón
+     * btn.setPrefSize(100, 60);
+     * btn.setStyle(
+     * "-fx-font-size: 18; " +
+     * "-fx-background-color: #4CAF50; " +
+     * "-fx-text-fill: white; " +
+     * "-fx-border-color: #45a049; " +
+     * "-fx-border-width: 2; " +
+     * "-fx-border-radius: 5; " +
+     * "-fx-background-radius: 5; " +
+     * "-fx-padding: 10 20; " +
+     * "-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.4), 10, 0, 0, 0);"
+     * );
+     * btn.setOnMouseEntered(new EventHandler<MouseEvent>() {
+     * 
+     * @Override public void handle(MouseEvent e) {
+     * btn.setStyle("-fx-background-color: #5EBE5E;"+
+     * "-fx-font-size: 18; " +
+     * "-fx-text-fill: white; ");
+     * }
+     * });
+     * btn.setOnMouseExited(new EventHandler<MouseEvent>() {
+     * 
+     * @Override public void handle(MouseEvent e) {
+     * btn.setStyle("-fx-background-color: #4CAF50;"+
+     * "-fx-font-size: 18; " +
+     * "-fx-text-fill: white; ");
+     * }
+     * });
+     * 
+     * btn.setOnAction(event -> {
+     * Ventana v = new Ventana();
+     * Ejercito e1 = new Ejercito(1);
+     * Ejercito e2 = new Ejercito(2);
+     * v.asignarAlTablero(e1);
+     * v.asignarAlTablero(e2);
+     * });
+     * 
+     * // Desplazar el botón "JUGAR" 110 píxeles hacia abajo
+     * btn.setTranslateY(110);
+     * 
+     * root.getChildren().add(btn);
+     * 
+     * Scene scene = new Scene(root);
+     * primaryStage.setScene(scene);
+     * 
+     * primaryStage.show();
+     * 
+     * }
+     */
 }
